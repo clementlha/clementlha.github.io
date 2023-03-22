2db8fe5e8ee1985e2b7ca6735925c5c3f63eb84e
CLEMENT LHARIDON <clement.lharidon@atos.net>
2023-03-22 17:25:05 +0100

    add files

---
+++ data/edit/scriptChartjs.js
@@ -0,0 +1,149 @@
+var ctx = document.getElementById("janvier2020");
+var myChart = new Chart(ctx, {
+  type: 'doughnut',
+  data: {
+    labels: ['Film & Animation', 'Autos & Vehicles', 'Music', 'Pets & Animals', 'Sports', 'Travel & Events', 'Gaming', 'People & Blogs','Comedy','Entertainment',	'News & Politics',	'Howto & Style',	'Education',	'Science & Technology',	'Nonprofits & Activism'
+  ],
+    datasets: [{
+      label: '# Nombre de vue total par catégorie',
+      data: [54296055,7306695,23681859,266081,26376567,2364955,39451377,4669391,46863546,52690622,38527083,16095206,9481511,3808287,51787],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.5)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)'
+      ],
+      borderColor: [
+        'rgba(255,99,132,1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)'
+      ],
+      borderWidth: 1
+    }]
+  },
+  options: {
+   	//cutoutPercentage: 40,
+    responsive: false,
+
+  }
+});
+
+var ctx = document.getElementById("fevrier2020");
+var myChart = new Chart(ctx, {
+  type: 'doughnut',
+  data: {
+    labels: ['OK', 'WARNING', 'CRITICAL', 'UNKNOWN'],
+    datasets: [{
+      label: '# of Tomatoes',
+      data: [12, 19, 3, 5],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.5)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)'
+      ],
+      borderColor: [
+        'rgba(255,99,132,1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)'
+      ],
+      borderWidth: 1
+    }]
+  },
+  options: {
+   	//cutoutPercentage: 40,
+    responsive: false,
+
+  }
+});
+var ctx = document.getElementById("mars2020");
+var myChart = new Chart(ctx, {
+  type: 'doughnut',
+  data: {
+    labels: ['OK', 'WARNING', 'CRITICAL', 'UNKNOWN'],
+    datasets: [{
+      label: '# of Tomatoes',
+      data: [12, 19, 3, 5],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.5)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)'
+      ],
+      borderColor: [
+        'rgba(255,99,132,1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)'
+      ],
+      borderWidth: 1
+    }]
+  },
+  options: {
+   	//cutoutPercentage: 40,
+    responsive: false,
+
+  }
+});
+
+var ctx = document.getElementById("avril2020");
+var myChart = new Chart(ctx, {
+  type: 'doughnut',
+  data: {
+    labels: ['OK', 'WARNING', 'CRITICAL', 'UNKNOWN'],
+    datasets: [{
+      label: '# of Tomatoes',
+      data: [12, 19, 3, 5],
+      backgroundColor: [
+        'rgb(255, 99, 132)',
+        'rgb(54, 162, 235)',
+        'rgb(255, 205, 86)',
+        'rgba(75, 192, 192, 0.2)'
+      ],
+      borderColor: [
+        'rgba(255,99,132,1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)'
+      ],
+      borderWidth: 1
+    }]
+  },
+  options: {
+   	//cutoutPercentage: 40,
+    responsive: false,
+
+  }
+});
+
+var ctx = document.getElementById("mai2020");
+var myChart = new Chart(ctx, {
+  type: 'doughnut',
+  data: {
+    labels: ['OK', 'WARNING', 'CRITICAL', 'UNKNOWN'],
+    datasets: [{
+      label: '# of Tomatoes',
+      data: [12, 19, 3, 5],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.5)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)'
+      ],
+      borderColor: [
+        'rgba(255,99,132,1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)'
+      ],
+      borderWidth: 1
+    }]
+  },
+  options: {
+   	//cutoutPercentage: 40,
+    responsive: false,
+
+  }
+});
